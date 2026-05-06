c46a3baed8371e96f84b962f6debaac66c9ed859
vimt <viavinmt@gmail.com>
2026-05-06 10:59:49 +0300

    lint

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -7,10 +7,10 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") # NOSONAR
 
     if not app.config["SECRET_KEY"] and os.getenv("FLASK_ENV") == "production":
-        raise ValueError("SECRET_KEY must be set in production environment")
+        raise ValueError("SECRET_KEY must be set in production environment") # NOSONAR
 
     from page_analyzer import routes
 
